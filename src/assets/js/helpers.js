7d751cc3ba30a2d9c463dce0912b45a13d014fb3
Wahyudin <whydn.id@hotmail.com>
2020-06-05 17:29:26 +0700

    Setup ice server

--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -105,21 +105,13 @@ export default {
 
 
     getIceServer() {
-        // "turns:eu-turn4.xirsys.com:5349?transport=tcp"
-        // "turns:eu-turn4.xirsys.com:443?transport=tcp"
-        // "turn:eu-turn4.xirsys.com:80?transport=tcp",
-        // "turn:eu-turn4.xirsys.com:3478?transport=udp",
         return {
             iceServers: [
                 {
-                    urls: ["stun:eu-turn4.xirsys.com"]
-                },
-                {
-                    username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
-                    credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
+                    username: "muazkh",
+                    credential: "webrtc@live.com",
                     urls: [
-                        "turn:eu-turn4.xirsys.com:80?transport=udp",
-                        "turn:eu-turn4.xirsys.com:3478?transport=tcp"
+                        "turn:numb.viagenie.ca",
                     ]
                 }
             ]
